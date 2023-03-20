fac5e1ca466c7f3ca261d1f597781146a7ae8351
Nicholas H.Tollervey <ntoll@ntoll.org>
2023-03-20 18:31:49 +0000

    Added working MicroPython example.

--- example2/pyscript.js
+++ example2/pyscript.js
@@ -1,24 +1,41 @@
 class PyScript extends HTMLElement {
     connectedCallback() {
-        const code = this.textContent;  // grab the source code.
-        this.textContent = "";  // Remove the code from the DOM.
-        // Handle STDOUT
-        document.addEventListener('micropython-print', function(e) {
-            this.textContent = this.textContent + "<code>" + e.output + "</code>"
-        }, false);
-        // Start up MicroPython
-        let mp_js_startup = Module['onRuntimeInitialized'];
-        Module["onRuntimeInitialized"] = async function() {
-            mp_js_startup();
-            mp_js_init(1024*1024);
-            mp_js_do_str(code);
-        }
+        // grab the source code.
+        const code = this.textContent;
+        // Remove the code from the DOM.
+        this.textContent = "";
+        // JavaScript oh my..! 🤦
+        const self = this;
+        // Handle STDOUT events.
+        document.addEventListener('micropython-print',
+            function(e) {
+                // Just append event data to the textual
+                // content of this element.
+                const content = self.textContent + e.data;
+                self.textContent = content;
+            },
+            false
+        );
+        // Eval the code.
+        mp_js_do_str(code);
     }
 }
 
-const interpreterElement = document.createElement("script");
-interpreterElement.src = "mpbuild/micropython.js";
-interpreterElement.onload = function(e) {
-    // Register the class for the given element name.
-    customElements.define("py-script", PyScript);
+
+// Inject MicroPython compiled to WASM into the page, via
+// a script tag.
+const pyElement = document.createElement("script");
+pyElement.src = "micropython.js";
+pyElement.onload = function(e) {
+    // Start up MicroPython
+    let mp_js_startup = Module['onRuntimeInitialized'];
+    Module["onRuntimeInitialized"] = async function() {
+        mp_js_startup();
+        mp_js_init(1024*1024);
+        // Register the class for the given element name,
+        // only after MicroPython is configured.
+        customElements.define("py-script", PyScript);
+    }
 }
+const head = document.getElementsByTagName('head')[0];
+head.appendChild(pyElement);
